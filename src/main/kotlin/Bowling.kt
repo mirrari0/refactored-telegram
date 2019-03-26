074f9ea54036a3552b1dd98bde19decdf65591d3
Steven Combs <scombs@pillartechnology.com>
2019-03-26 00:35:41 -0500

    Finished kata

--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -1,7 +1,48 @@
-
-
-fun List<Ball>.scoreGame() : Int {
+fun List<Ball>.scoreGame(): Int {
     var result = 0
-    forEach {result += it.roll.toInt() }
+    var rollCount = 0
+    forEachIndexed { index, ball ->
+        if(rollCount < 20) {
+            result += scoreBall(index, ball, this)
+            if(ball.isStrike()) rollCount+=2
+            else rollCount++
+        }
+    }
     return result
+}
+
+fun scoreBall(index: Int, ball: Ball, game: List<Ball>): Int {
+    return when {
+        ball.isSpare() -> scoreSpare(index, game)
+        ball.isStrike() -> scoreStrike(index, game)
+        else -> ball.roll.toInt()
+    }
+}
+
+private fun getNextBallValue(index: Int, game: List<Ball>):Int{
+    return if(game.size >index) {
+        when {
+            game[index].isStrike() -> 10
+            game[index].isSpare() -> 10 - game[index-1].roll.toInt()
+            else -> game[index].roll.toInt()
+        }
+    }
+    else 0
+}
+
+
+private fun scoreStrike(index: Int, game: List<Ball>) : Int {
+    return 10 + getNextBallValue(index+1, game) + getNextBallValue(index+2, game)
+}
+
+private fun scoreSpare(index: Int, game: List<Ball>) : Int {
+    return 10 - game[index - 1].roll.toInt() + getNextBallValue(index+1, game)
+}
+
+private fun Ball.isSpare(): Boolean {
+    return "/" == roll
+}
+
+private fun Ball.isStrike(): Boolean {
+    return "X".equals(roll, true)
 }
--- src/test/kotlin/BowlingTests.kt
+++ src/test/kotlin/BowlingTests.kt
@@ -11,11 +11,36 @@ class BowlingTests {
 
     @Test
     fun `given a game with 20 rolls scoring one should return twenty`() {
-        assertEquals(20, generateGame("1",20).scoreGame())
+        assertEquals(20, generateGame("1", 20).scoreGame())
+    }
+
+    @Test
+    fun `given a game with 21 rolls scoring 5 each should return two hundred`() {
+        val game = generateGame("5", 21)
+        for (i in 1..20 step 2) {
+            game[i] = Ball("/")
+        }
+        assertEquals(150, game.scoreGame())
+    }
+
+    @Test
+    fun `given a game with 13 rolls scoring 10 each should return three hundred`() {
+        assertEquals(300, generateGame("X", 13).scoreGame())
+    }
+
+    @Test
+    fun `given a game with 17 rolls scoring 10 each frame should return 195`() {
+        val game = generateGame("X", 4)
+        for (i in 0..12 step 2) {
+            game.add(Ball("5"))
+            game.add(Ball("/"))
+        }
+        game.add(Ball("5"))
+        assertEquals(195, game.scoreGame())
     }
 
 
-    private fun generateGame(score: String, numberOfBalls: Int): List<Ball> {
+    private fun generateGame(score: String, numberOfBalls: Int): MutableList<Ball> {
         val game = mutableListOf<Ball>()
         repeat(numberOfBalls) {
             game.add(Ball(score))
